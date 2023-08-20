a986f012e4cb34e5d4314bfb60e123240b45843e
Md. Jobayer Hossain <jobayerhossain298@gmail.com>
2023-08-20 20:13:39 +0600

    first commit

--- src/Loader/Loader.jsx
+++ src/Loader/Loader.jsx
@@ -1,19 +1,11 @@
 import React from 'react';
 import { FidgetSpinner } from 'react-loader-spinner';
+import img from './giphy.gif'
 
 const Loader = () => {
     return (
-      <div className="h-screen grid place-items-center">
-        <FidgetSpinner
-          visible={true}
-          height="100"
-          width="100"
-          ariaLabel="dna-loading"
-          wrapperStyle={{}}
-          wrapperClass="dna-wrapper"
-          ballColors={["#ff0000", "#00ff00", "#0000ff"]}
-          backgroundColor="#F4442E"
-        />
+      <div className="h-screen bg-black grid place-items-center">
+        <img src={img} alt="" />
       </div>
     );
 };
